6ebc59ec69b2b29eb0ce3d565a1b1039239c0277
skylar <shkylar@gmail.com>
2025-03-29 15:25:02 -0700

    chore: update dependencies and environment configuration

--- src/utils/vehicle/index.ts
+++ src/utils/vehicle/index.ts
@@ -1,6 +1,6 @@
-
 // Re-export all the functionality
 export * from './types';
 export * from './import-parser';
 export * from './database-operations';
 export * from './image-operations';
+export * from './management-utils';
