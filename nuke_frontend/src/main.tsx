4ecc0f537d09579414125ab6c83666b4ebad60a8
skylar williams <shkylar@gmail.com>
2025-10-16 15:28:26 -0700

    Add minimal app test to isolate Vercel build issues

--- nuke_frontend/src/main.tsx
+++ nuke_frontend/src/main.tsx
@@ -1,14 +1,14 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import './index.css'
-import App from './App.tsx'
+import MinimalApp from './MinimalApp.tsx'
 
 // Debug: verify that main.tsx is executing in the browser
-console.log('[main] starting application bootstrap');
+console.log('[main] starting minimal application bootstrap');
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
-    <App />
+    <MinimalApp />
   </StrictMode>,
 )
 
