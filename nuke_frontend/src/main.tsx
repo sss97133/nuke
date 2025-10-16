cc4851c8a4f74ca59a97d7d6d16ec9bddbcfeb61
skylar williams <shkylar@gmail.com>
2025-10-16 15:29:48 -0700

    Restore full app with aggressive chunk splitting to resolve Vercel build memory limits

--- nuke_frontend/src/main.tsx
+++ nuke_frontend/src/main.tsx
@@ -1,14 +1,14 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import './index.css'
-import MinimalApp from './MinimalApp.tsx'
+import App from './App.tsx'
 
 // Debug: verify that main.tsx is executing in the browser
-console.log('[main] starting minimal application bootstrap');
+console.log('[main] starting application bootstrap');
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
-    <MinimalApp />
+    <App />
   </StrictMode>,
 )
 
